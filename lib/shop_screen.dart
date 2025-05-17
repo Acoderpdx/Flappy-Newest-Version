84a62b77d0c2d547e51dd454a83f3395173d5061
Aidan Bernard <aidanbernard@Aidans-MacBook-Pro.local>
2025-05-16 17:12:59 -0700

    Fix: Matrix shop switches are now vertical and next to skins

--- lib/shop_screen.dart
+++ lib/shop_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ShopScreen extends StatelessWidget {
+class ShopScreen extends StatefulWidget {
   final int lionsManeCollected;
   final int redPillCollected;
   final int bitcoinCollected;
@@ -20,24 +20,37 @@ class ShopScreen extends StatelessWidget {
   });
 
   @override
+  State<ShopScreen> createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  late String _selectedSkin;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedSkin = widget.currentBirdSkin;
+  }
+
+  @override
   Widget build(BuildContext context) {
     final skins = [
       {
         'filename': 'tate_bird.png',
         'collectible': 'RedPill',
-        'count': redPillCollected,
+        'count': widget.redPillCollected,
         'asset': 'assets/images/tate_bird.png',
       },
       {
         'filename': 'lion_bird.png',
         'collectible': 'LionsMane',
-        'count': lionsManeCollected,
+        'count': widget.lionsManeCollected,
         'asset': 'assets/images/lion_bird.png',
       },
       {
         'filename': 'bankman_bird.png',
         'collectible': 'Bitcoin',
-        'count': bitcoinCollected,
+        'count': widget.bitcoinCollected,
         'asset': 'assets/images/bankman_bird.png',
       },
     ];
@@ -45,37 +58,62 @@ class ShopScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset('assets/images/matrix_shop.png', fit: BoxFit.cover, width: double.infinity, height: double.infinity),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: skins.map((skin) {
-                final unlocked = unlockedSkins.contains(skin['filename'] as String);
-                final canUnlock = skin['count'] as int >= 10;
-                Widget skinImage = Image.asset(
-                  skin['asset'] as String,
-                  width: 80,
-                  height: 80,
-                  color: (!unlocked && !canUnlock) ? Colors.black : null,
-                  colorBlendMode: (!unlocked && !canUnlock) ? BlendMode.srcATop : null,
-                );
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 18.0),
-                  child: Column(
-                    children: [
-                      skinImage,
-                      if (!unlocked && canUnlock)
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: ElevatedButton(
-                            onPressed: () => onUnlock(skin['filename'] as String, skin['collectible'] as String),
+          Image.asset(
+            'assets/images/matrix_shop.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: skins.map((skin) {
+                  final filename = skin['filename'] as String;
+                  final unlocked = widget.unlockedSkins.contains(filename);
+                  final canUnlock = skin['count'] as int >= 10;
+                  Widget skinImage = Image.asset(
+                    skin['asset'] as String,
+                    width: 80,
+                    height: 80,
+                    color: (!unlocked && !canUnlock) ? Colors.black : null,
+                    colorBlendMode: (!unlocked && !canUnlock) ? BlendMode.srcATop : null,
+                  );
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 18.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        skinImage,
+                        SizedBox(width: 24),
+                        if (!unlocked && canUnlock)
+                          ElevatedButton(
+                            onPressed: () => widget.onUnlock(filename, skin['collectible'] as String),
                             child: Text('Unlock'),
                           ),
-                        ),
-                    ],
-                  ),
-                );
-              }).toList(),
+                        if (unlocked)
+                          RotatedBox(
+                            quarterTurns: 1,
+                            child: Switch(
+                              value: filename == _selectedSkin,
+                              onChanged: (val) {
+                                if (val) {
+                                  setState(() {
+                                    _selectedSkin = filename;
+                                  });
+                                  widget.onEquip(filename);
+                                }
+                              },
+                              activeColor: Colors.green,
+                              inactiveThumbColor: Colors.white,
+                              inactiveTrackColor: Colors.grey,
+                            ),
+                          ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           ),
           Positioned(
